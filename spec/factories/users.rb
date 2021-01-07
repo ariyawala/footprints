0700739ca52192a59c9b2e6cc4a986667b39e751
ariyawala <ojima574@gmail.com>
2021-01-07 16:01:33 +0900

    単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'aaa111' }
+    password_confirmation { password }
+    birthday              { '1930-1-1' }
   end
 end
